7af167a72a020c0c55d9e5c195391cf73fb3bedc
KrishnaveniPujari <krishnaveni_pujari@persistent.com>
2022-01-27 21:10:22 +0530

    added fun headers

--- math.py
+++ math.py
@@ -1,14 +1,14 @@
-
+# Add implementation
 def add(x,y) :
      pass 
-     
+# Sub Impl    
 def substract(x,y) :
      pass
 
-	 
+# Multiply  impl	 
 def multiply(x,y) :
      pass
 
-
+# Divide Impl
 def divide(x,y) :
      pass
